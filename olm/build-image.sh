9f8142b6639cf8e38bd2be11fc62358a6140f276
Felix Maldonado <felix.maldonado@oracle.com>
2026-03-05 15:29:48 -0600

    oracle-template: add Jenkinsfile, template, build image script, security

---
+++ olm/build-image.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
+CONTAINER_CLI="${CONTAINER_CLI:-podman}"
+
+name="cluster-api-provider-vsphere"
+version="{{{ .major }}}.{{{ .minor }}}.{{{ .patch }}}"
+registry="container-registry.oracle.com/olcne"
+docker_tag=${registry}/${name}:v${version}
+
+"${CONTAINER_CLI}" build --pull \
+    --build-arg https_proxy=${https_proxy:-} \
+    --build-arg http_proxy=${http_proxy:-} \
+    --volume /etc/yum.repos.d:/etc/yum.repos.d \
+    --tag ${docker_tag} -f ./Dockerfile .
